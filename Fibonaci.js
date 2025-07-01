2dd0f156923247e10210dca32b435d46b21774e4
unknown <vasanth.a842000@gmail.com>
2025-07-01 13:27:09 +0530

    comments added

--- Fibonaci.js
+++ Fibonaci.js
@@ -1,3 +1,5 @@
+//fibonaci - take first number and second number add two number you will get third number now make it as 
+// assign second number value to first number and third number value assign to second number
 function fibonaci(count){
     let first=0;
     let second=1;
