45f551aae24b299466396fca792a858fd10f2c59
vasanth842000 <vasanth.a842000@gmail.com>
2025-08-16 18:30:22 +0530

    promise all settled

---
+++ Javascript/promiseallsettled.js
@@ -0,0 +1,27 @@
+//promise.allettled - wait for all promises to be settled(either it can be fullfilled or rejected ) ,  gives the result of each promises status / value / reason .
+
+function MovieTicketbooking(){
+    return new Promise((res,rej)=>{
+        setTimeout(() => {
+            res('movie ticket is booked..');
+        }, 2000);
+    })
+}
+
+function AmountWithdrawn(){
+    return new Promise((res,rej)=>{
+        setTimeout(() => {
+            res('amount was withdraw sucessfully');
+        }, 3000);
+    })
+}
+
+function cabBooking(){
+    return new Promise((res,rej)=>{
+        setTimeout(() => {
+            rej('cab was bookedd successfully.')
+        }, 3000);
+    })
+}
+
+Promise.allSettled([MovieTicketbooking(),AmountWithdrawn(),cabBooking()]).then((results)=>console.log(results)).catch((err)=>console.log(err))
